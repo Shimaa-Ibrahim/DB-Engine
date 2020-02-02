194b696fe5f8183f79a80b059b84b45868342861
shimaa <shimaa@192.168.1.4>
2020-02-02 20:43:04 +0200

    tableMenu to be Updated

---
+++ tableMenu.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash 
+shopt -s extglob
+function tableMenu {
+clear
+select choice in "List Tables" "Create Table" "DELETE Table" "INSERT into Table" "DELETE RECORD" "SELECT from Table" "RETURN TO MAIN MENU"
+do
+	case $choice in
+	"List Tables")
+	clear      
+	echo "*** TABLES LIST***"   
+	ls -1
+
+	;;
+
+	"Create Table")
+	./createTable.sh
+	;;
+
+	"DELETE Table")
+	;;
+
+	"INSERT into Table")
+	;;
+
+	"DELETE RECORD")
+	;;
+
+	"SELECT from Table")
+	;;
+
+	"RETURN TO MAIN MENU")
+	./MainMenu.sh
+	;;
+	*)
+	 clear
+ 	 echo "WARNING::Wrong choice, please select from 1 to 7"
+	 echo "----PRESS ANY KEY TO RETURN TO THE MAIN MENU----"
+	 read -s
+	 tableMenu;;
+
+esac
+done
+
+}
+tableMenu
