b2ecc16f2dc14032cc78c640f0f68490bf4fffd6
basmmohamed <basmasalah2017@gmail.com>
2020-02-04 15:59:21 +0200

    add validation to createtable function on colname

--- createTable.sh
+++ createTable.sh
@@ -11,20 +11,20 @@ read tname
 while [ 1 -eq 1 ]
 	do
                 #here fix this by add dbname before table name
-		if [[ -f ~/DB-Engine/$tname ]]
+		if [[ -f ~/DB-Engine/$tableName ]]
 		then
-			echo "$tname Already EXISTS"
-			read tname
-		elif [[ -z $tname  ]]
+			echo "$tableName Already EXISTS"
+			read tableName
+		elif [[ -z $tableName  ]]
 		then
-			echo "$tname not valid enter a valid name"
+			echo "$tableName not valid enter a valid name"
 			echo "elif 1"
-			read tname
-		elif [[ ! $tname =~ $string ]]
+			read tableName
+		elif [[ ! $tableName =~ $string ]]
 		then
-			echo "$tname  not valid"
+			echo "$tableName  not valid please enter valid name string charaacters"
 			echo "elif 2"
-			read tname
+			read tableName
 		else 
 			echo "please enter columns number"
 			read numCol
@@ -37,7 +37,27 @@ while [ 1 -eq 1 ]
 							colMetadata="";
                                                         echo "Enter column name: "
 							read  colName;
-                                                        #add colname to colmetadate so that append on it datatype 
+                                                       
+#check if field(colName) already exists or not in the file but not working because file not created yet
+							if grep -Fxq "$colName" $tableName.metaData
+							then
+								echo "$colName Already EXISTS"
+								i=$i-1
+								continue;
+							elif [[ -z $colName  ]]
+							then
+								echo "$colName not valid enter a valid name from letters"
+								echo "elif 1"
+								i=$i-1
+								continue;
+							elif [[ ! $colName =~ $string ]]
+							then
+								echo "$colName  not valid must be dtring character"
+								echo "elif 2"
+								i=$i-1
+								continue;
+							else                                                        
+							#add colname to colmetadate so that append on it datatype 
 					        	colMetadata="$colName";
 							echo "please enter column datatype String(s) TntegerNumber(n): (s/n)" 
 							read colDataType;
@@ -49,12 +69,12 @@ while [ 1 -eq 1 ]
 								colMetadata="$colMetadata:number"
 							fi
                                                         #here fix this by add dbname before table name
-							echo  "$colMetadata" >> ~/DB-Engine/$tname.metaData
+							echo  "$colMetadata" >> ~/DB-Engine/$tableName.metaData
 							
 						done
                                         #here fix this by add dbname before table name
-					touch ~/DB-Engine/$tname
-					touch ~/DB-Engine/$tname.metaData
+					touch ~/DB-Engine/$tableName
+					touch ~/DB-Engine/$tableName.metaData
 					if [[ $? -eq 0 ]]
 					then
 						echo "table created successfully"
