45a6828cd1ca31951ae37e81566e7e5ae19ff1c8
basmmohamed <basmasalah2017@gmail.com>
2020-02-09 14:39:45 +0200

    add delete record with delete all option

--- deleteRecord.sh
+++ deleteRecord.sh
@@ -2,19 +2,12 @@
 shopt -s extglob
 
 function deleteRecord {
-<<<<<<< HEAD
 clear
-=======
-
->>>>>>> 94cd9512bf93dcf69b0c3d6f6b57305bfeb3c78f
 	echo "please enter the table name to delete from"
 	read tableName
 while [ 1 -eq 1 ]
  do
-<<<<<<< HEAD
 	clear
-=======
->>>>>>> 94cd9512bf93dcf69b0c3d6f6b57305bfeb3c78f
 		if [ ! -f $tableName ] && [ ! -z $tableName ]
 		then 
 			echo "inValid table name does not exist please enter a valid table name"
@@ -23,30 +16,21 @@ while [ 1 -eq 1 ]
 		then
 			echo "Empty Input! Please, enter a valid name"
 			read tableName
-<<<<<<< HEAD
 		else
 
-=======
-		elif [ -f $tableName ]
-		then
->>>>>>> 94cd9512bf93dcf69b0c3d6f6b57305bfeb3c78f
 			numCol=`cat $tableName.metaData | wc -l`
 			#echo $numCol
 			declare -a colNames
 			declare -a colValues
 			rowNum=`cat $tableName | wc -l`
 			colNameExist=0
-<<<<<<< HEAD
 			colValueExist=0
-=======
->>>>>>> 94cd9512bf93dcf69b0c3d6f6b57305bfeb3c78f
 			for (( i=1; i<=numCol; i++ ))
 			do
 				colName=`sed -n "$i p" $tableName.metaData | cut -d: -f1`
 				colNames[$i-1]=$colName
 				
 			done
-<<<<<<< HEAD
                         echo "Do you want to delete all records or only one record press [a|o]?"
 			read answer
 			if  [[ $answer == "a" || $answer == "A" ]]
@@ -126,61 +110,3 @@ function deleteAll {
 	
 	done
 }
-=======
-
-			echo "please enter column name to delete a record according to it from the following"
-			echo ${colNames[@]}
-			read colNameToDeleteAcc
-					
-			for (( i=1; i<=numCol; i++ ))
-			do
-				if [[ ${colNames[$i-1]} == $colNameToDeleteAcc ]]
-				then
-					colNameExist=1
-					fieldNum=$i
-					break;
-				
-				fi
-			done
-			
-			if [[ colNameExist -eq 0 ]]
-			then
-				echo "in valid column name"				
-				break;
-			elif [[ colNameExist=1 ]]
-			then
-					
-			for (( i=2; i<=rowNum; i++ ))
-			do
-				colValue=`sed -n "$i p" $tableName | cut -d ' ' -f$fieldNum`
-				colValues[$i-1]=$colValue
-				
-			done
-			echo ${colValues[@]}
-			echo "please enter column value to delete a record according to it"
-			read colValueToBeDeleted
-			rowNum=`cat $tableName | wc -l`
-			
-			for (( i=2; i<=rowNum; i++ ))
-			 do
-				if [[ ${colValues[$i-1]} == $colValueToBeDeleted ]]
-				then
-					
-					
-					`sed -i "$i d" $tableName`
-         				echo "Record where $colNameToDeleteAcc = $colValueToBeDeleted has been deleted"
-					rowNum=$rowNum-1
-					#to delete 1st value match
-					break;
-				
-				fi
-			 done
-			#to exit the while loop after delete the record
-			break;
-			fi
-		  
-		  fi	
- done
-}
-
->>>>>>> 94cd9512bf93dcf69b0c3d6f6b57305bfeb3c78f
